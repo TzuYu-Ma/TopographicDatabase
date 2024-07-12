609762f20958dadb17396e5d103e2d1583c541bc
TzuYu-Ma <tzuyuma20@gmail.com>
2024-07-12 02:35:16 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -135,4 +135,4 @@ def download_file(filename):
 
 if __name__ == "__main__":
     create_select_function()  # Create the function when the app starts
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
